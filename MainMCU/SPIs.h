58cbea9ffd302de3116e7ada7681de4ea1d0489a
Rabe1402 <huberrabe@gmail.com>
2026-03-10 13:27:23 +0100

    finsih spi connection with documentation. and wrote code for image calculations with claude to get a relativ angle from center and aproximate distanc for the ball. exmaple for MainMcu code dan be found in SPIs.h

--- MainMCU/SPIs.h
+++ MainMCU/SPIs.h
@@ -1,20 +1,27 @@
-#here are all the spi sonnections to the main mcu. 
-#This file is included in the main mcu code.
-#The spi communication is only used to recieve information
-#There are four SPI ports on the mainboard, SPICAM, SPI1, SPI2, and SPI3.
-#SPICAM is used to read the camera Data.
-#SPI2 is used to read the Ground sensors 
+#include "../shared/KeplerBRAIN_V4.h"
+//here are all the spi sonnections to the main mcu. 
+//This file is included in the main mcu code.
+//The spi communication is only used to recieve information
+//There are four SPI ports on the mainboard, SPICAM, SPI1, SPI2, and SPI3.
+//SPICAM is used to read the camera Data.
+//SPI2 is used to read the Ground sensors 
 
+//Example für Winkel von Kamera zurück rechnen. 
+    //uint8_t rel_angle  = (int8_t)(SPICAM_Data2 - 90);  // -35..+35°
+    //float  target     = imu_heading + rel_angle;
+
+    //if (target <   0) target += 360;
+    //if (target >= 360) target -= 360;
 
 #spi Variables: 
-uint8_t SPICAM_Data0 = 0;
-uint8_t SPICAM_Data1 = 0;
-uint8_t SPICAM_Data2 = 0;
-uint8_t SPICAM_Data3 = 0;
-uint8_t SPICAM_Data4 = 0;
-uint8_t SPICAM_Data5 = 0;
-uint8_t SPICAM_Data6 = 0;
-uint8_t SPICAM_Data7 = 0;
+uint8_t SPICAM_Data0 = 0; //this is the trigger for the reading sequence (250 in cam code) 
+uint8_t SPICAM_Data1 = 0; // boolean for if there is a Ball in sight
+uint8_t SPICAM_Data2 = 0; // relative angle to the ball (0-180, 90 is straight ahead, 0 is left, 180 is right)
+uint8_t SPICAM_Data3 = 0; // distance estimate
+uint8_t SPICAM_Data4 = 0; // area MSB
+uint8_t SPICAM_Data5 = 0; // area LSB
+uint8_t SPICAM_Data6 = 0; // elongation
+uint8_t SPICAM_Data7 = 0; // reserved
 
 uint8_t SPI2_Data0 = 0;
 uint8_t SPI2_Data1 = 0;
@@ -48,8 +55,6 @@ void _SPIs()
  
   // read 8 Bytes from OpenMV END
  
-  }
-  {
     // read 8 Bytes from Ground Sensors BEGIN
 
     digitalWrite(SPI2, LOW);
@@ -67,4 +72,3 @@ void _SPIs()
     }
   }
 }
-
--- cam/cam.py
+++ cam/cam.py
@@ -4,15 +4,15 @@ import pyb
 import time
 import math
 
-led_green = pyb.LED(2)  # Green LED on the OpenMV Cam
-led_red = pyb.LED(1)  # Red LED on the OpenMV Cam
-led_blue = pyb.LED(3)  # Blue LED on the OpenMV Cam
-led_ir = pyb.LED(4)  # IR LED on the OpenMV Cam
+led_green = pyb.LED(2)
+led_red = pyb.LED(1)
+led_blue = pyb.LED(3)
+led_ir = pyb.LED(4)
 
-spi_list = [250, 0, 0, 0, 0, 0, 0, 0]  # SPI data list to send to the microcontroller. The first value is the SPI command, the rest are data values.
-spi_data = bytearray(spi_list)  # Convert the SPI data list to a bytearray for sending.
-threshold_index = 0  # 0 for red
- 
+spi_list = [250, 0, 0, 0, 0, 0, 0, 0]
+spi_data = bytearray(spi_list)
+threshold_index = 0
+
 def nss_callback(line):
     global spi, spi_data
     try:
@@ -23,84 +23,94 @@ def nss_callback(line):
         led_green.off()
         led_red.on()
         pass
- 
+
 pyb.ExtInt(pyb.Pin("P3"), pyb.ExtInt.IRQ_FALLING, pyb.Pin.PULL_UP, nss_callback)
 
-
-# Color Tracking Thresholds (L Min, L Max, A Min, A Max, B Min, B Max)
-# The below thresholds track in general red/green/blue things. You may wish to tune them...
 thresholds = [
-  #  (42, 77, 10, 53, 53, 127),
-  #  (0, 77, 9, 73, -11, 127),
-  #  (0, 77, 9, 73, 13, 127),
-  #  (37, 57, 30, 54, 12, 59)
-  #  (37, 57, 30, 54, 12, 69)
-  #  (11, 41, 4, 50, 1, 28)
     (40, 73, 33, 96, -31, 61)
- 
-]  # generic_blue_thresholds
- 
+]
+
 sensor.reset()
 sensor.set_pixformat(sensor.RGB565)
 sensor.set_framesize(sensor.QVGA)
 sensor.skip_frames(time=2000)
-sensor.set_auto_gain(False)  
-sensor.set_auto_whitebal(False)  
+sensor.set_auto_gain(False)
+sensor.set_auto_whitebal(False)
 clock = time.clock()
- 
-# Only blobs that with more pixels than "pixel_threshold" and more area than "area_threshold" are
-# returned by "find_blobs" below. Change "pixels_threshold" and "area_threshold" if you change the
-# camera resolution. "merge=True" merges all overlapping blobs in the image.
- 
+
+FRAME_W    = 320
+FRAME_H    = 240
+H_FOV_HALF = 35.0  # halbes horizontales FOV (~70° gesamt)
+
 while True:
     clock.tick()
     img = sensor.snapshot()
     blobs = img.find_blobs(
-    [thresholds[threshold_index]],
-    pixels_threshold=20,
-    area_threshold=10,
-    merge=True,
+        [thresholds[threshold_index]],
+        pixels_threshold=20,
+        area_threshold=10,
+        merge=True,
     )
-    biggestblobarea=0
- 
+
+    biggestblobarea = 0
+    biggestblob = None
+
     for blob in blobs:
-        # These values depend on the blob not being circular - otherwise they will be shaky.
         if blob.elongation() > 0.4:
             img.draw_edges(blob.min_corners(), color=(255, 0, 0))
             img.draw_line(blob.major_axis_line(), color=(0, 255, 0))
             img.draw_line(blob.minor_axis_line(), color=(0, 0, 255))
- 
+
         blobarea = blob.area()
-        if(blobarea > biggestblobarea):
+        if blobarea > biggestblobarea:
             biggestblobarea = blobarea
             biggestblob = blob
-        # These values are stable all the time.
+
         img.draw_rectangle(blob.rect())
         img.draw_cross(blob.cx(), blob.cy())
-        # Note - the blob rotation is unique to 0-180 only.
         img.draw_keypoints(
             [(blob.cx(), blob.cy(), int(math.degrees(blob.rotation())))], size=20
         )
-        if(biggestblobarea > 0):
-            print(str(biggestblob.cx()) + "A" + str(biggestblob.cy()) )
-        else:
-            print("B")
 
-    #Image Calculations (move as much calculation on to the openmv cam as possible to save time on the microcontroller)
+    # Image Calculations
+    if biggestblobarea > 0 and biggestblob is not None:
+        # Relativer Winkel: negativ = Ball links, positiv = Ball rechts
+        norm_x  = (biggestblob.cx() - FRAME_W / 2.0) / (FRAME_W / 2.0)
+        angle_h = int(norm_x * H_FOV_HALF)  # ca. -35 bis +35°
+
+        # Distanzschätzung über Blob-Höhe (größerer Blob = näher = höherer Wert)
+        dist_byte = max(0, min(255, int((biggestblob.h() / FRAME_H) * 255)))
+
+        # Area auf zwei Bytes
+        area_clamped = min(biggestblobarea, 65535)
+        area_hi = (area_clamped >> 8) & 0xFF
+        area_lo =  area_clamped       & 0xFF
+
+        # Winkel mit Offset +90 als uint8 senden (90 = geradeaus)
+        angle_byte = max(0, min(255, angle_h + 90))
+
+        spi_list[1] = 1           # Ball erkannt
+        spi_list[2] = angle_byte  # rel. Winkel (MCU: angle_h = spi[2] - 90)
+        spi_list[3] = dist_byte   # Distanzschätzung
+        spi_list[4] = area_hi     # Area MSB
+        spi_list[5] = area_lo     # Area LSB
+        spi_list[6] = max(0, min(255, int(biggestblob.elongation() * 100)))
+        spi_list[7] = 0           # reserviert
+
+        print("Ball: angle={} dist={}".format(angle_h, dist_byte))
+    else:
+        spi_list[1] = 0   # kein Ball
+        spi_list[2] = 90  # Mitte als Default → MCU rechnet 90-90=0, kein Drift
+        spi_list[3] = 0
+        spi_list[4] = 0
+        spi_list[5] = 0
+        spi_list[6] = 0
+        spi_list[7] = 0
+
+        print("Kein Ball")
+
+    # SPI Data Publishing
+    spi_data = bytearray(spi_list)
+
+    print(clock.fps())
     
-
-
-    #SPI Data Publishing
-    
-    #spi_list[1] = <some value calculated from the image>
-    #spi_list[2] = <some value calculated from the image>
-    #spi_list[3] = <some value calculated from the image>
-    #spi_list[4] = <some value calculated from the image>
-    #spi_list[5] = <some value calculated from the image>
-    #spi_list[6] = <some value calculated from the image>
-    #spi_list[7] = <some value calculated from the image>
-    spi_data = bytearray(spi_list)  # Update the SPI data bytearray with
-    print(spi_data) # Print the SPI data for debugging purposes. #remove this line in production.
-
-
-    print (clock.fps())  # Print the FPS 
