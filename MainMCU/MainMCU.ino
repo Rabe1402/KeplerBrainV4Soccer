252a8a2cd1d160b9cc958ee2e35b3a5f76063223
Rabe1402 <huberrabe@gmail.com>
2026-03-17 13:19:52 +0100

    yaaaayyyy cam works, now letys try to drive behind the ball

--- MainMCU/MainMCU.ino
+++ MainMCU/MainMCU.ino
@@ -268,7 +268,7 @@ void _default(){
     WRITE_LCD_TEXT(1,1, String(ball_target) + " " + String(_cam_data_calculation()));
     rotate_to_quadratic(ball_target, 2, 23, 0.0000004, 0.00000004, 0);
   
-    //move_angle(_cam_data_calculation(), 40);
+    move_angle(_cam_data_calculation(), 40);
   }
   WRITE_LCD_TEXT(1, 2, String(counter));
 }
